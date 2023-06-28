0102fc84781157c3e4d96e3cd21acc6cddc065cd
bengris32 <bengris32@protonmail.ch>
2023-06-28 16:35:54 +0700

    treewide: Remove usages of the `{CONFIG_}FACTORY_BUILD` macro
    Change-Id: Id2d346b96427ddcf3d32d5f6837bef5443f07bda
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Aeron-Aeron <aeronrules2@gmail.com>

--- drivers/misc/mediatek/include/mt-plat/v1/charger_type.h
+++ drivers/misc/mediatek/include/mt-plat/v1/charger_type.h
@@ -7,13 +7,8 @@
 #define __MTK_CHARGER_TYPE_H__
 
 #include <linux/device.h>
-#ifdef CONFIG_FACTORY_BUILD
-#define SDP_ICL		600
-#define SDP_FCC		500
-#else
 #define SDP_ICL		500
 #define SDP_FCC		500
-#endif
 #define SDP_VINMIN	4200
 
 #define CDP_ICL		1500
